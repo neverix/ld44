f70d951a55de2c04dde81a0e7876c72cc00d312a
Stepan Shabalin <stomperhomp@gmail.com>
2019-04-27 09:08:25 +0700

    basic main loop

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import { ECS, JobSystem } from "@eix/core"
 import { spriteRenderingJob, spriteComponent } from "@eix/gfx"
 import { Portal } from "@eix/ui"
+import MainLoop from "mainloop.js";
+
+const ecs = new ECS()
+const jobSystem = new JobSystem()
+jobSystem.addTask("update", ecs)
+jobSystem.tasks.update
+    .addJob("log", (ecs) => {
+        return (delta: number) => { }
+    })
+jobSystem.addTask("draw", ecs)
+
+MainLoop
+    .setUpdate(delta => jobSystem.tasks.update.runJobs(delta))
+    .setDraw(interpolationPercentage => jobSystem.tasks.draw.runJobs(interpolationPercentage))
+    .start()
