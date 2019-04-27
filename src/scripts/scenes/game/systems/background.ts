6e1c3e2e1d6422290e4b54db0296eae4e3f5c961
Stepan Shabalin <stomperhomp@gmail.com>
2019-04-27 21:30:38 +0700

    removed unnecessary stuff

--- src/scripts/scenes/game/systems/background.ts
+++ src/scripts/scenes/game/systems/background.ts
@@ -7,17 +7,10 @@ export const Background: System = (jobSystem) => {
         return (_e: any) => {
             const background = new Image()
             background.src = require("../../../../../img/forestbg.jpg")
-            const id = ecs.addEntity()
-            ecs.all.is(id)
-                .pipe((ids: number[], ecs: ECS) => {
-                    ids.forEach(id => {
-                        ecs.entities[id]["pos"] = 'e'
-                    })
-                    return ids
-                })
+            ecs.addEntityFlowGroup()
                 .addComponent("sprite", { image: background })
                 .addComponent("position", vec2.fromValues(0, 0))
-                .addComponent("rotation", 0.5)
+                .addComponent("rotation", 0)
                 .addComponent("scale", vec2.fromValues(background.width, background.height))
                 .get()
         }
