0b0ef83fdb4736b9feff230f74ee152bb3bd71f5
Stepan Shabalin <stomperhomp@gmail.com>
2019-04-29 23:40:12 +0700

    added null/undefined check

--- src/scripts/scenes/game/systems/enemies/enemyAnimation.ts
+++ src/scripts/scenes/game/systems/enemies/enemyAnimation.ts
@@ -12,31 +12,33 @@ export const enemyAnimation: System = (js: JobSystem) => {
         //return a function
         return (delta: number) => {
             components.tracked.forEach((entity: any) => {
-                //increase timer
-                entity.timer += delta
+                if (!!entity) {
+                    //increase timer
+                    entity.timer += delta
 
-                //get timestamp
-                const minTimeStamp = 1000 / entity.enemy.fps
+                    //get timestamp
+                    const minTimeStamp = 1000 / entity.enemy.fps
 
-                // console.log(entity.timer)
+                    // console.log(entity.timer)
 
-                if (minTimeStamp < entity.timer) {
-                    //reset timer
-                    entity.timer = 0
+                    if (minTimeStamp < entity.timer) {
+                        //reset timer
+                        entity.timer = 0
 
-                    //get index of new image
-                    const index = entity.images.indexOf(
-                        entity.drawable.drawableContent.image
-                    ) + 1
+                        //get index of new image
+                        const index = entity.images.indexOf(
+                            entity.drawable.drawableContent.image
+                        ) + 1
 
-                    // console.log(index)
+                        // console.log(index)
 
-                    //update image
-                    entity.drawable.drawableContent.image = entity.images[
-                        (index >= entity.images.length) ? 0 : index
-                    ]
+                        //update image
+                        entity.drawable.drawableContent.image = entity.images[
+                            (index >= entity.images.length) ? 0 : index
+                        ]
 
-                    // console.log(entity.drawable.drawableContent)
+                        // console.log(entity.drawable.drawableContent)
+                    }
                 }
             })
         }
